bf648f9e8f16cb29be4f525f8b9fdbfb39e005b5
Brandon Droelle <bmdroelle@gmail.com>
2023-04-10 15:10:06 -0400

    Added functionality to the drop down menu
    When a class is created the name of the class is now added to the drop down menu.

--- CSI3370Project/Assets/Scripts/profileButtons.cs
+++ CSI3370Project/Assets/Scripts/profileButtons.cs
@@ -8,8 +8,11 @@ public class profileButtons : MonoBehaviour
 {
     //public attributes
     public List<ProfileClass> profileList = new List<ProfileClass>();   //creates a list of profileClass objects
+    public List<string> profileNames = new List<string>();              //creates list of profile names
     public int profileIndex = 0;            //holds index for current class
     public int numOfProfiles = 0;           //counts number of profiles
+
+    public GameObject dropdownMenu;         //contains the dropdown menu
 
     public string theText;                  //contain the new profiles name
     public GameObject newNameTextBox;       //contains the text box
@@ -53,6 +56,7 @@ public class profileButtons : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        print("start program");
         //initialize sprites
         avatarArray[0] = avatar001;
         avatarArray[1] = avatar002;
@@ -65,6 +69,10 @@ public class profileButtons : MonoBehaviour
         //set canvases
         createProfileCanvas.SetActive(true);
         viewProfileCanvas.SetActive(false);
+
+        //load data
+        //profileList = PlayerPrefs.GetList("profileObjects");
+        //profileNameList = PlayerPrefs.GetList("profileNames");
 
     }
 
@@ -145,36 +153,89 @@ public class profileButtons : MonoBehaviour
     //delete current profile
     public void deleteProfile()
     {
+        //checks if there are more than 0 profiles
+        //if there are then removes profile object from list
         if(numOfProfiles > 0)
         {
-            profileList.RemoveAt(profileIndex);
-            numOfProfiles -= 1;
+            profileList.RemoveAt(profileIndex);     //removes profile object at current index
+            profileNames.RemoveAt(profileIndex);    //removes profile name at current index
+            numOfProfiles -= 1;                     //removes 1 from count of total profiles
             print("deleted profile");
         }
         else
             print("no profiles to delete");
     }
 
+    //fill drop down with list of profile names
+    public void fillDropdown()
+    {
+        print("in fillDropdown");
+        //initialize drop down menu
+        var dropdown = dropdownMenu.GetComponent<TMP_Dropdown>();
+
+        print("dropdown object created");
+        //clear dropdown
+        dropdown.options.Clear();
+
+        foreach(var name in profileNames)
+        {
+            dropdown.options.Add(new TMP_Dropdown.OptionData() { text = name});
+        }
+
+        dropdownItemSelected(dropdown);
+
+        dropdown.onValueChanged.AddListener(delegate { dropdownItemSelected(dropdown);});
+
+    }
+
+    //change dropdown selection to name of item in list
+    void dropdownItemSelected(TMP_Dropdown dropdown)
+    {
+        print("in dropdownItemSelected");
+        int index = dropdown.value;
+
+        updateViewProfile(index);
+    }
+
     //save profile button
     public void saveProfileButton()
     {
+        print("save profile button clicked");
+        //gets name of profile from textbox
         theText = newProfileName.GetComponent<TMP_Text>().text;
-
+        //creates new profile class object
         ProfileClass tempProfile = new ProfileClass(theText, currentAvatarIndex, currentBackgroundIndex);
 
-        string testName = tempProfile.getName();
-        int testAvatar = tempProfile.getAvatar();
-        int testBackground = tempProfile.getBackground();
-
         profileList.Add(tempProfile);                   //append new profile to list
+        profileNames.Add(theText);                      //append name of profile to list of names
         numOfProfiles += 1;                             //add 1 to count of profiles
 
         //reset canvas
         resetCanvas();
 
+        //update dropdown
+        fillDropdown();
+        print("out of fillDropdown");
+
+        //save data
+        saveData();
+
         //switch canvases
         viewProfileCanvas.SetActive(true);
         createProfileCanvas.SetActive(false);
         
+    }
+
+    public void updateViewProfile(int index)
+    {
+        currentBackgroundIndex = profileList[index].getBackground();
+        currentAvatarIndex = profileList[index].getAvatar();
+    }
+
+    //save data
+    public void saveData()
+    {
+        //PlayerPrefs.SetList("profileObjects", profileList);
+        //PlayerPrefs.SetString("profileNames_count", profileNames.Count);
     }
 }
